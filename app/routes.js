4380711b64d8ccedf198e380c9efe3e2adfde3af
Paula Stephenson <paulacstephenson@gmail.com>
2018-04-20 15:16:51 +0100

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var router = express.Router()
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+// add your routes here
+
+router.post('/juggling-balls-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'juggling-balls'
+  var jugglingBalls = req.session.data['juggling-balls']
+
+  // Check whether the variable matches a condition
+  if (jugglingBalls == "3 or more"){
+    // Send user to next page
+    res.redirect('/juggling-trick')
+  } else {
+    res.redirect("/ineligible")
+  }
+})
+
+
+module.exports = router
